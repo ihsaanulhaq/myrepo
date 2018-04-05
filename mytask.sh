27e164f351f974c15feee169174cbf75b809faa5
M.Ihsan-ul-Haq <xflow@xflowresearch.com>
2018-04-05 03:17:48 -0400

    milestone-3 attempt1

--- mytask.sh
+++ mytask.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+function f1(){
 cd /home/xflow/ovs
 ovs-vsctl --version
 RESULT=$?
@@ -13,13 +14,18 @@ if [ $RESULT == 0 ]; then
     break
   done
 fi
+}
 
+function f2(){
 #remove old, clone & install afresh
 cd /home/xflow
 rm -rf /home/xflow/ovs
 
 
 git clone https://github.com/openvswitch/ovs.git
+}
+
+function f3(){
 cd ovs
 if ! [ -x "$(command -v autoreconf)" ]; then
   echo 'Error: autoreconf is not installed.' >&2
@@ -37,5 +43,33 @@ if [ $RESULT == 0 ]; then
 else
   echo Installation failed...
 fi
+}
+
+function f4(){
+
+echo milestone-2.....
+
+ovs-vsctl del-br mybr1
+ovs-vsctl add-br mybr1 -- set Bridge mybr1 fail-mode=secure
+ovs-vsctl add-port mybr1 ens5
+ovs-ofctl add-flow mybr1 "in_port=1,ip,nw_src=192.168.10.191,actions=drop"
+#ovs-ofctl dump-tables-desc mybr1
+}
+
+function f5(){
+for ((i=1; i<=100; i++))
+do
+  ovs-ofctl add-flow mybr1 "in_port=1,ip,nw_src=192.168.10.$i,actions=drop"
+  #echo rule-$i installed...
+done
+ovs-ofctl dump-flows mybr1
+echo Execution finished....
+}
+
+f1
+f2
+f3
+f4
+f5
 
 
